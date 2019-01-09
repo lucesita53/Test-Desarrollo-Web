ba9e9612902871ad9ad4f65869a5bbf7230b978c
Luciana Moll (Pi Data Strategy LucianaMoll(PiDataSt Consulting) <lmoll@piconsulting.com.ar>
2019-01-09 15:57:25 -0300

    [Test-1][Create main screen - FE] Add images

--- business_company/src/Components/ShowImages.js
+++ business_company/src/Components/ShowImages.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import '../App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
+
+import facebook from '../Images/Facebook.png';
+import apple from '../Images/Apple.jpg';
+import microsoft from '../Images/Microsoft.png';
+import google from '../Images/Google.png';
+import amazon from '../Images/Amazon.png';
 
 class ShowImages extends Component {
   render() {
     return (
-        <div className="Images">
-            <Text>Imagenes</Text>
-        </div>
+        <Carousel showThumbs={false} showArrows={true} showStatus={true} autoPlay={true} >
+            <div>
+                <img src={facebook}/>
+            </div>
+            <div>
+                <img src={apple}/>
+            </div>
+            <div>
+                <img src={microsoft}/>
+            </div>
+            <div>
+                <img src={google}/>
+            </div>
+            <div>
+                <img src={amazon}/>
+            </div>
+        </Carousel>
     );
   }
 }
