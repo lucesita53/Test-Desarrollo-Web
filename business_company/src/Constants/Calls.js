720988fdf7b00ba4d8515ae3a7e8d08b40e5212d
Luciana Moll (Pi Data Strategy LucianaMoll(PiDataSt Consulting) <lmoll@piconsulting.com.ar>
2019-01-11 00:02:03 -0300

    [Test-3][Call service] Add call service and show results on screen

--- business_company/src/Constants/Calls.js
+++ business_company/src/Constants/Calls.js
@@ -1,43 +1,25 @@
-var Request = require("request");
+const symbolCompanies = ['FB', 'AAPL', 'MSFT', 'GOOGL', 'AMZN']
 
-Request.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FB&outputsize=compact&apikey=X86NOH6II01P7R24", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
+export default class Calls {
+    
+    async callWebService(onSuccess, symbolCompany)  {
+        if (!symbolCompanies.includes(symbolCompany)) {
+            return;
+        }
+        var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbolCompany + '&outputsize=compact&apikey=X86NOH6II01P7R24';
+                   fetch(url)
+                       .then(response => response.json())
+                       .then(obj => obj['Time Series (Daily)'])
+                       .then(dailies => {
+                           let date = new Date();
+                           let month = (date.getMonth() +1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() +1);
+                           let key = date.getFullYear() + '-' + month + '-' + date.getDate();
+                           return dailies[key];})
+                        .then(result =>{
+                            onSuccess({'open' : result['1. open'],
+                            'high' :result['2. high'],
+                            'low' : result['3. low'],
+                            'close' : result['4. close']});
+                        });
     }
-    console.dir(JSON.parse(body));
-});
-
-Request.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&outputsize=compact&apikey=X86NOH6II01P7R24", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-});
-
-Request.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=compact&apikey=X86NOH6II01P7R24", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-});
-
-Request.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOGL&outputsize=compact&apikey=X86NOH6II01P7R24", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-});
-
-Request.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&outputsize=compact&apikey=X86NOH6II01P7R24", (error, response, body) => {
-	if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-});
-
-Request.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FB&outputsize=compact&apikey=X86NOH6II01P7R24", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-});
+}
